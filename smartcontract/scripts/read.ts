d2878f7e4ae68104297193b22a218489c0337a01
KTNguyen04 <championvi12@gmail.com>
2025-11-30 23:13:35 +0700

    edit read.ts

--- smartcontract/scripts/read.ts
+++ smartcontract/scripts/read.ts
@@ -1,2 +1,10 @@
-const factory = await ethers.getContractFactory("UniversityDegreesSBT");
-console.log(factory.interface.format());
+import fs from "fs";
+
+const abi = JSON.parse(
+  fs.readFileSync(
+    "./artifacts/contracts/UniversityDegreesSBT.sol/UniversityDegreesSBT.json",
+    "utf8"
+  )
+).abi;
+
+console.log(abi);
